401ad194fb0c6966d2624f6b06c3bb5edab08194
Xinsong Lin <x8lin@ucsd.edu>
2024-06-12 00:24:10 -0700

    tuned start and stop

--- mani_skill/envs/tasks/tabletop/slide_cube.py
+++ mani_skill/envs/tasks/tabletop/slide_cube.py
@@ -18,7 +18,7 @@ from mani_skill.utils.structs.pose import Pose
 class SlideCubeEnv(BaseEnv):
     SUPPORTED_ROBOTS = ["panda_pizza",]
     agent: PandaPizza
-    cube_half_size = 0.02
+    cube_half_size = 0.015
 
     def __init__(self, *args, robot_uids="panda_pizza", robot_init_qpos_noise=0.02, **kwargs):
         self.robot_init_qpos_noise = robot_init_qpos_noise
@@ -31,7 +31,7 @@ class SlideCubeEnv(BaseEnv):
 
     @property
     def _default_human_render_camera_configs(self):
-        pose = sapien_utils.look_at([0.6, 0.7, 0.6], [0.0, 0.0, 0.35])
+        pose = sapien_utils.look_at([0.6, 0.7, 1.6], [0.0, 0.0, 0.35])
         return CameraConfig("render_camera", pose, 512, 512, 1, 0.01, 100)
 
     def _load_scene(self, options: dict):
@@ -43,17 +43,30 @@ class SlideCubeEnv(BaseEnv):
             self.scene, half_size=self.cube_half_size, color=[1, 0, 0, 1], name="cube"
         )
 
+    def _generate_cube_init_pos(self):
+        """ get a batch of cube poses """
+        p = self.agent.tcp.pose.p
+        b = p.shape[0]
+        # then find the x and y axis of the pizza peel by reading their quaternion
+        tf = self.agent.tcp.pose.to_transformation_matrix()
+        x_axis = tf[..., :3, 0]
+        y_axis = tf[..., :3, 1]
+        z_axis = tf[..., :3, 2]
+        # randomly shift p along x and y axis a little bit
+        p += x_axis * (torch.rand(b) - 0.5) * 0.2
+        p += y_axis * (torch.rand(b) - 0.5) * 0.2
+        # set z to be just above the pizza peel by the height of the cube
+        p += z_axis * (self.cube_half_size + 0.02)
+        return p
+
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
         with torch.device(self.device):
             b = len(env_idx)
             self.table_scene.initialize(env_idx)
-            xyz = torch.zeros((b, 3))
-            xyz[:, :2] = torch.rand((b, 2)) * 0.2 - 0.1
-            xyz[:, 2] = self.cube_half_size +0.5
+            xyz = self._generate_cube_init_pos()
             qs = randomization.random_quaternions(b, lock_x=True, lock_y=True)
             self.cube.set_pose(Pose.create_from_pq(xyz, qs))
             
-
     def _get_obs_extra(self, info: Dict):
         # in reality some people hack is_grasped into observations by checking if the gripper can close fully or not
         obs = dict(
@@ -62,15 +75,28 @@ class SlideCubeEnv(BaseEnv):
         if "state" in self.obs_mode:
             obs.update(
                 obj_pose=self.cube.pose.raw_pose,
-                tcp_to_obj_pos=self.cube.pose.p - self.agent.tcp.pose.p,
+                tcp_to_obj_pos=self.cube.pose.p - self.agent.tcp.pose.p,  # how far it is to the hole
             )
         return obs
 
+    def _signed_distance_cube_to_pizza_peel(self):
+        tf = self.agent.tcp.pose.to_transformation_matrix()
+        z_axis = tf[..., :3, 2]
+        return torch.sum((self.cube.pose.p - self.agent.tcp.pose.p) * z_axis, dim=1)
+
+    def _distance_cube_to_pizza_peel_hole(self):
+        return torch.linalg.norm(
+            self.cube.pose.p - self.agent.tcp.pose.p, axis=1
+        )
+
     def evaluate(self):
-        is_robot_static = self.agent.is_static(0.2)
+        # success is if distance is less than the cube size and cube has signed distance less than 0
+        success = self._distance_cube_to_pizza_peel_hole() <= self.cube_half_size
+        # print(self._distance_cube_to_pizza_peel_hole(), self._signed_distance_cube_to_pizza_peel())
+        success = success & (self._signed_distance_cube_to_pizza_peel() < self.cube_half_size + 0.01 + 1e-5)
         return {
-            "success": is_robot_static,
-            "is_robot_static": is_robot_static,
+            "success": success,
+            "is_robot_static": self.agent.is_static(0.2),
         }
 
     def compute_dense_reward(self, obs: Any, action: torch.Tensor, info: Dict):
